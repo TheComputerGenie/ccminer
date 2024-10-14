ef2c259f5b2ca89c85ae31b151f201cd64c497af
ComputerGenie <tim@yourcomputergenie.com>
2024-10-14 12:06:01 -0500

    update config

--- build.sh
+++ build.sh
@@ -16,8 +16,8 @@ aclocal && autoheader && automake --add-missing --gnu --copy && autoconf || echo
 
 extracflags="-march=native -D_REENTRANT -falign-functions=16 -falign-jumps=16 -falign-labels=16"
 
-CUDA_CFLAGS="-O3 -lineno -Xcompiler -Wall  -D_FORCE_INLINES" \
-	./configure CXXFLAGS="-O3 $extracflags" --with-cuda=/usr/local/cuda --with-nvml=libnvidia-ml.so
+CUDA_CFLAGS="-O3 -lineno -Xcompiler -Wall -D_FORCE_INLINES" \
+	./configure CXXFLAGS="-O3 $extracflags -std=c++17" --with-cuda=/usr/local/cuda --with-nvml=libnvidia-ml.so
 
 
-make -j 4
+make -j 20
