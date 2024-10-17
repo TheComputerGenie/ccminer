9dd8e9383c48aeda50c5fc2e1e15505eb5216001
ComputerGenie <tim@yourcomputergenie.com>
2024-10-17 14:00:12 -0500

    cleanup

--- build.sh
+++ build.sh
@@ -2,22 +2,15 @@
 
 # Simple script to create the Makefile and build
 
-# export PATH="$PATH:/usr/local/cuda/bin/"
-
 make distclean || echo clean
 
-rm -f Makefile.in
-rm -f config.status
+rm -f Makefile.in config.status
 
-aclocal && autoheader && automake --add-missing --gnu --copy && autoconf || echo done
+#aclocal && autoheader && automake --add-missing --gnu --copy && autoconf || echo done
+#vs
+autoreconf -i
+#🤦‍♂️️
+CUDA_CFLAGS="-O3 -Xcompiler -Wall -D_FORCE_INLINES" \
+	./configure --with-nvml=libnvidia-ml.so
 
-# CFLAGS="-O2" ./configure
-# To change the cuda arch, edit Makefile.am and run ./build.sh
-
-extracflags="-march=native -D_REENTRANT -falign-functions=16 -falign-jumps=16 -falign-labels=16"
-
-CUDA_CFLAGS="-O3 -lineno -Xcompiler -Wall -D_FORCE_INLINES" \
-	./configure CXXFLAGS="-O3 $extracflags -std=c++17" --with-cuda=/usr/local/cuda --with-nvml=libnvidia-ml.so
-
-
-make -j 20
+make ccminerGPU -j20
